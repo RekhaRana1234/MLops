3fa31da580f24954fc37b7405a7e9dbc119f1996
RekhaRana1234 <36226993+RekhaRana1234@users.noreply.github.com>
2022-06-22 15:12:50 +0200

    Update data_download.py

--- data_download.py
+++ data_download.py
@@ -1,8 +1,19 @@
 import requests
+from PIL import Image
+
 
 response = requests.get("https://i.imgur.com/ExdKOOz.png")
 
 file = open("sample_image.png", "wb")
 file.write(response.content)
 file.close()
+
+im1 = Image.open("sample_image.png")
+
+
+#show images
+im1.show()
+
+
+
 print("Job Completed of downloading the image!!!")
